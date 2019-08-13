fb36459b2f0693c4d47e546ac3da07e4eba06a67
mobinhassani <mobinhassani9374@gmail.com>
2019-08-13 11:03:26 +0430

    first commit

---
+++ JwtSample.Api/Controllers/AuthController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JwtSample.Api.Controllers
+{
+    public class AuthController : Controller
+    {
+        [Route("login")]
+        public IActionResult Login()
+        {
+            return Ok(GenerateJSONWebToken());
+        }
+
+        private string GenerateJSONWebToken()
+        {
+            var claims = new List<Claim>();
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("mobinmahdi.ir/Key"));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: "mobinmahdi.ir/Issuer",
+                audience: "mobinmahdi.ir/Audience",
+                claims: claims,
+                notBefore: DateTime.UtcNow,
+                expires: DateTime.Now.AddYears(1),
+                signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
---
+++ JwtSample.Api/Controllers/DataController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwtSample.Api.Controllers
+{
+    [Authorize]
+    public class DataController : Controller
+    {
+        [Route("data")]
+        public IActionResult Index()
+        {
+            return Ok("mobin");
+        }
+    }
+}
